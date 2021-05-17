f1ec6869a89cea0bfa5136bd1c6d4ca1dbd43240
Dino Tudor <dinotudor@gmail.com>
2021-05-17 08:48:32 -0300

    Home page and global css fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Inicio | ig.news</title>
-    </Head>
-   <h1>Hello Mundão!</h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React World</span></h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
    </>
   )
 }
